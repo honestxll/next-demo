c21ccc3227ef1d3883864fed578fd0f25d61a0dd
Honest <honestxll@gmail.com>
2020-05-25 12:53:24 +0800

    添加 bootstrap 到项目全局样式中
    使用 bootstrap 做为主要样式框架

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,13 @@ import Link from "next/link";
 
 export default function IndexPage() {
   return (
-    <div>
-      Hello World. <Link href="/about">About</Link>
+    <div className="container my-5">
+      <h1>你好!</h1>
+      <div className="alert alert-info">
+        <Link href="/about">
+          <a>About Page!</a>
+        </Link>
+      </div>
     </div>
   );
 }
